c7aff87e26a21a7ac2db93d402020544c78d7103
D3fau4 <raulwii2@gmail.com>
2022-12-27 12:02:28 +0100

    add c/h files

---
+++ xdelta3.c
@@ -0,0 +1,9 @@
+//
+// Created by raul2 on 27/12/2022.
+//
+
+#include "xdelta3.h"
+
+void hello(){
+
+}
---
+++ xdelta3.h
@@ -0,0 +1,10 @@
+//
+// Created by raul2 on 27/12/2022.
+//
+
+#ifndef XDELTA3_NX_XDELTA3_H
+#define XDELTA3_NX_XDELTA3_H
+
+void hello();
+
+#endif //XDELTA3_NX_XDELTA3_H
