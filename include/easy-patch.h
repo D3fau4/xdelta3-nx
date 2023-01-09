2e297ba019c6c3b53b96f302cff80b35d9836eed
Raúl Álvarez <raulwii2@gmail.com>
2023-01-09 14:05:08 +0100

    Easy patch skeleton

---
+++ include/easy-patch.h
@@ -0,0 +1,9 @@
+#ifndef XDELTA3_NX_EASY_PATCH_H
+#define XDELTA3_NX_EASY_PATCH_H
+
+#include <iostream>
+#include <switch.h>
+
+int patch(std::string inPath, std::string outPath, std::string patchPath);
+
+#endif //XDELTA3_NX_EASY_PATCH_H
---
+++ source/easy-patch.cpp
@@ -0,0 +1,9 @@
+#include "../include/easy-patch.h"
+
+int patch(std::string inPath, std::string outPath, std::string patchPath) {
+    FILE *infile;
+    FILE *outfile;
+    FILE *patch;
+
+    return 0;
+}
